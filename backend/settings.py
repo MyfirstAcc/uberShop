a8264cdb8657590dcd0b5dc70f6d52f0572b29d7
denario <densuper2005@gmail.com>
2023-12-25 16:20:21 +0300

    Create build frontend

--- backend/settings.py
+++ backend/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
     }
 }
 
-# Database
+# Подключение сторенней БД
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 # DATABASES = {
 #    'default': {
@@ -161,7 +161,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
